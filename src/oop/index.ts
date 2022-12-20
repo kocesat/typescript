2d7f94a1718ee58319405861927a2bf17315268d
kocesat <kocesat@gmail.com>
2022-12-21 00:47:19 +0300

    generic classes

--- src/oop/index.ts
+++ src/oop/index.ts
@@ -150,6 +150,15 @@ class MyCalendar implements ICalendar {
 }
 
 
+// Generic classes
+class KeyValuePair<K, V> {
+  constructor(public key: K, public value: V) {}
+}
+
+let pair = new KeyValuePair<string, string>('a', 'b');
+
+
+
 
 
 
